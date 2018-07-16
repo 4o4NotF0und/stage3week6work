1f5b844d6c28c2f77cdb63c8121acf255afdc2b1
4o4NotF0und <1039071825@qq.com>
2018-07-16 22:43:54 +0800

    初始的版本v1.0

---
+++ entry.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import store from './store/index.js'
+
+import App from './App.jsx'
+
+function show () {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+show ()
+
+store.subscribe(show)
